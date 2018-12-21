9a02df94629ce2b89c4787b99012611d739e1e2b
Dolphy Phan <dolphy@ebi.ac.uk>
2018-12-21 14:59:10 +0000

    update

--- images/upload.sh
+++ images/upload.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-for i in hhvltxfer00{1..9}; do scp 10-multus.conf  $i:/etc/cni/net.d/; done
-scp 10-multus.conf hhvltxfer010:/etc/cni/net.d/
-#for i in hhvltxfer00{1..9}; do ssh $i "systemctl restart kubelet"; done
-#ssh hhvltxfer010 "systemctl restart kubelet"
+for i in hhvltxfer00{1..9}; do scp 15-multus.conf  $i:/etc/cni/net.d/;scp multus.kubeconfig  $i:/etc/cni/net.d/multus.d/;done
+scp 15-multus.conf hhvltxfer010:/etc/cni/net.d/
+scp multus.kubeconfig hhvltxfer010:/etc/cni/net.d/multus.d/
+
+# for i in hhvltxfer00{1..9}; do ssh $i "mv /etc/cni/net.d/10-calico.conflist /etc/cni/net.d/20-calico.conflist"; done
+# ssh hhvltxfer010 "mv /etc/cni/net.d/10-calico.conflist /etc/cni/net.d/20-calico.conflist"
+
+for i in hhvltxfer00{1..9}; do ssh $i "systemctl restart kubelet"; done
+ssh hhvltxfer010 "systemctl restart kubelet"
+kubectl delete pod pod --force --grace-period=0
+kubectl apply -f multus-daemonset.yml
