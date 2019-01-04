cf6c8b1c48b44528dd8046987955e75395426707
Dolphy Phan <dolphy@ebi.ac.uk>
2019-01-04 11:29:22 +0000

    update all

--- images/upload.sh
+++ images/upload.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-for i in hhvltxfer00{1..9}; do scp 15-multus.conf  $i:/etc/cni/net.d/;scp multus.kubeconfig  $i:/etc/cni/net.d/multus.d/;done
+for i in hhvltxfer00{1..9}; do scp 15-multus.conf  $i:/etc/cni/net.d/;done
 scp 15-multus.conf hhvltxfer010:/etc/cni/net.d/
-scp multus.kubeconfig hhvltxfer010:/etc/cni/net.d/multus.d/
+# scp multus.kubeconfig hhvltxfer010:/etc/cni/net.d/multus.d/
 
-# for i in hhvltxfer00{1..9}; do ssh $i "mv /etc/cni/net.d/10-calico.conflist /etc/cni/net.d/20-calico.conflist"; done
-# ssh hhvltxfer010 "mv /etc/cni/net.d/10-calico.conflist /etc/cni/net.d/20-calico.conflist"
+
+# for i in hhvltxfer00{1..9}; do scp 00-multus.conf  $i:/etc/cni/net.d/;done
+# scp 00-multus.conf hhvltxfer010:/etc/cni/net.d/
+
+for i in hhvltxfer00{1..9}; do ssh $i "rm -rif /etc/cni/net.d/00-multus.conf"; done
+ssh hhvltxfer010 "rm -rif /etc/cni/net.d/00-multus.conf"
 
 for i in hhvltxfer00{1..9}; do ssh $i "systemctl restart kubelet"; done
 ssh hhvltxfer010 "systemctl restart kubelet"
-kubectl delete pod pod --force --grace-period=0
-kubectl apply -f multus-daemonset.yml
+# kubectl delete pod pod --force --grace-period=0
+# kubectl apply -f multus-daemonset.yml
+kubectl -n ftp delete pod pod
